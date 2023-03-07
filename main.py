6a8ded6a8615871c782a7a7eca46bb95ea02077f
Александр Михешкин <miheshs@mail.ru>
2023-03-07 22:28:18 +0300

    code according to PEP8

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name, char_class):
+    """Функция подсчёта урона от атаки."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +17,7 @@ def attack(char_name, char_class):
 
 
 def defence(char_name, char_class):
+    """Функция подсчёта заблокированного урона."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +27,7 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
+    """Функция подсчёта специального умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +38,7 @@ def special(char_name, char_class):
 
 
 def start_training(char_name, char_class):
+    """Функция выполнения тренировки."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +63,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Функция выбора класса персонажа."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -78,7 +85,8 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
@@ -89,5 +97,3 @@ def main():
     char_class = choice_char_class()
     print(start_training(char_name, char_class))
 
-
-main()
